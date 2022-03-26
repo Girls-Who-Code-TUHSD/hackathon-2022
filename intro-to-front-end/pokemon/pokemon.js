0c568cce83c7350118c6ee33992b31d0c2f41cd3
alex <meetalexjohnson@gmail.com>
2022-03-26 15:37:09 -0700

    Actual live coding code

--- intro-to-front-end/pokemon/pokemon.js
+++ intro-to-front-end/pokemon/pokemon.js
@@ -1,24 +1,25 @@
-const mainDiv = document.getElementById('pokemons')
+// TODO: request list of pokemon from API
+const apiURL = 'https://pokeapi.co/api/v2/pokemon?limit=20/'
 
-// https://developer.mozilla.org/en-US/docs/Web/API/fetch
-const fetchPokemon = fetch('https://pokeapi.co/api/v2/pokemon?limit=20/')
+const getPokemon = fetch(apiURL)
 
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/then
-fetchPokemon.then(res => {
-  // https://developer.mozilla.org/en-US/docs/Web/API/Response/json
-  res.json().then(pokemons => {
+getPokemon.then(response => {
+  response.json().then(pokemons => {
     pokemons.results.forEach(pokemon => {
+      // TODO: go through list of pokemon, adding each one to the DOM
+      // Make a link element
       const link = document.createElement('a')
+      // add text to be displayed for this link
       link.innerText = pokemon.name
+      // add URL that link points to
       link.href = pokemon.url
 
-      const node = document.createElement('p')
+      const node = document.createElement('div')
       node.appendChild(link)
 
+      // add the elements we just made to the document
+      const mainDiv = document.getElementById('pokemons')
       mainDiv.appendChild(node)
     })
   })
 })
-
-// TODO: when link is clicked, fetch results at URL and add to DOM.
