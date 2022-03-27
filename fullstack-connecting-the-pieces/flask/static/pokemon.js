3b69bc4cfb49f9fdccac77d9c788f5213a274bbd
Timothy Stapleton <retrocede@outlook.com>
2022-03-26 17:33:05 -0700

    add code pulling FE and BE together, and working for a heroku deploy

---
+++ fullstack-connecting-the-pieces/flask/static/pokemon.js
@@ -0,0 +1,26 @@
+// TODO: request list of pokemon from API
+//const apiURL = 'https://pokeapi.co/api/v2/pokemon?limit=20/'
+const apiURL = 'http://localhost:5000/api/v1/resources/pokemon/all'
+
+const getPokemon = fetch(apiURL)
+
+getPokemon.then(response => {
+  response.json().then(pokemons => {
+    pokemons.pokemon.forEach(pokemon => {
+      // TODO: go through list of pokemon, adding each one to the DOM
+      // Make a link element
+      const link = document.createElement('a')
+      // add text to be displayed for this link
+      link.innerText = pokemon.name
+      // add URL that link points to
+      link.href = pokemon.url
+
+      const node = document.createElement('div')
+      node.appendChild(link)
+
+      // add the elements we just made to the document
+      const mainDiv = document.getElementById('pokemons')
+      mainDiv.appendChild(node)
+    })
+  })
+})
